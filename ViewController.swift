f6eea3d79944059a1b9f8389eb19eccd9f100629
Dzhami <rakhmetov.dzhami@yandex.ru>
2023-10-02 12:46:54 +0700

    init

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HeadsHandsTest
+//
+//  Created by Dzhami on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
